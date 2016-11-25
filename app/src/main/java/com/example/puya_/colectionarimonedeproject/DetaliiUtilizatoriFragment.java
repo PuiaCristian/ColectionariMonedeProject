2d519387e043e06f79ea90571c4ca59ce1a6ae6d
rusuvalentin <rusuvalentin95@yahoo.com>
2016-11-25 16:33:38 +0200

    commit

---
+++ app/src/main/java/com/example/puya_/colectionarimonedeproject/DetaliiUtilizatoriFragment.java
@@ -0,0 +1,23 @@
+package com.example.puya_.colectionarimonedeproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rusuvalentin on 25/11/2016.
+ */
+
+public class DetaliiUtilizatoriFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_detalii_utilizatori,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/puya_/colectionarimonedeproject/Utilizatori.java
+++ app/src/main/java/com/example/puya_/colectionarimonedeproject/Utilizatori.java
@@ -1,7 +1,9 @@
 package com.example.puya_.colectionarimonedeproject;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Utilizatori extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class Utilizatori extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_utilizatori);
+
+        int screenOrintation= getResources().getConfiguration().orientation;
+
+        if(screenOrintation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            hideSidePanel();
+        }
+
+
+    }
+
+    private void hideSidePanel(){
+        View sidePane= findViewById(R.id.fragment_lista_utilizatori);
+        if(sidePane.getVisibility() == View.VISIBLE)
+        {
+            sidePane.setVisibility(View.GONE);
+        }
     }
 }
